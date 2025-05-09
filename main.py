28f12316bc8c166d43952e46763affac98168ac3
Shresta Mallina <Shrestamallina.s@gmail.com>
2025-05-09 10:25:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import requests
 import smtplib
 TARGET_PRICE =100
 SUBJECT ="Amazon Price Alert!"
-MY_EMAIL = "shrestamallina.s@gmail.com"
+MY_EMAIL = "your mail"
 PASSWORD = "ghsp vyag jywj zamc"
-LANGUAGE = "en-IN,en;q=0.9,te-IN;q=0.8,te;q=0.7,en-GB;q=0.6,en-US;q=0.5"
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+LANGUAGE = "HTTP header"
+USER_AGENT = "Http Header"
 LINK = "https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1"
 response = requests.get(url=LINK,headers={"Accept-Language":LANGUAGE,
                                           "User-Agent":USER_AGENT})
@@ -27,5 +27,5 @@ if prices < TARGET_PRICE:
     connection = smtplib.SMTP(host="smtp.gmail.com", port=587)
     connection.starttls()
     connection.login(user=MY_EMAIL, password=PASSWORD)
-    connection.sendmail(from_addr=MY_EMAIL, to_addrs="mallinamuralikrishna@gmail.com",
+    connection.sendmail(from_addr=MY_EMAIL, to_addrs="12345@gmail.com",
                         msg=f"subject:{SUBJECT}\n\n{subject} now ${prices}.{decimal_price}\n{LINK}.".encode(encoding="utf-8"))
